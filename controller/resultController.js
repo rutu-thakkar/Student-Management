e92f2a10bd7e24e4781431cd7e27cb38e3e70ffa
Rutu Thakkar <rutu.thakkar@iviewlabs.net>
2022-01-11 18:34:20 +0530

    addresult

--- controller/resultController.js
+++ controller/resultController.js
@@ -6,13 +6,33 @@ exports.addmarks = (req, res) => {
             enrollNo: req.body.enrollNo
         }
     }).then((data) => {
-        db.result.create({
-            enrollNo: req.body.enrollNo,
-            class: req.body.class,
-            semester: req.body.semester,
-            maths: req.body.maths,
-            science: req.body.science,
-            english: req.body.english
+        db.result.findOne({
+            where: {
+                enrollNo: req.body.enrollNo,
+                class: req.body.class,
+                semester: req.body.semester
+            }
+        }).then((data) => {
+            if (data) {
+                res.send("Can't Enter details as It is already entered")
+            } else {
+                db.result.create({
+                    enrollNo: req.body.enrollNo,
+                    class: req.body.class,
+                    semester: req.body.semester,
+                    maths: req.body.maths,
+                    science: req.body.science,
+                    english: req.body.english
+                }).then((data) => {
+                    if (!data) {
+                        res.send("No data Found");
+                    } else {
+                        res.send(data)
+                    }
+                }).catch((error) => {
+                    res.send("error : " + error)
+                })
+            }
         })
     }).catch((error) => {
         res.send("Error : " + error)
